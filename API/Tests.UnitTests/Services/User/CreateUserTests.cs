1d1039fcbc14b3549c93b8dbb471991323893e04
Luiz-Kohler <luizfelipekohler03@gmail.com>
2023-04-29 12:35:33 -0300

    test: creating test from user creation

--- API/Tests.UnitTests/Services/User/CreateUserTests.cs
+++ API/Tests.UnitTests/Services/User/CreateUserTests.cs
@@ -1,15 +1,16 @@
-﻿using Application.Common.Hash;
+﻿using Application.Common.Exceptions;
+using Application.Common.Hash;
 using Application.Common.JWT;
 using Application.Services.User.Create;
 using AutoMapper;
 using Bogus;
-using Domain.Entities;
 using Domain.IRepositories;
 using FizzWare.NBuilder;
 using FluentAssertions;
-using Moq;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 using System.Linq.Expressions;
+using Address = Domain.Entities.Address;
 
 namespace Tests.UnitTests.Services.User
 {
@@ -67,6 +68,30 @@ namespace Tests.UnitTests.Services.User
             _jwtHandler.Received(1).CreateToken(Arg.Any<Domain.Entities.User>());
             _hashHandler.Received(1).Hash(Arg.Any<string>());
             response.Token.Should().Be(expectedToken);
+        }
+
+        [Fact]
+        public async Task Should_Return_Error_When_Already_Exists_User_With_Same_Email()
+        {
+            var user = Builder<Domain.Entities.User>.CreateNew().Build();
+
+            var request = Builder<CreateUserRequest>
+                .CreateNew()
+                .With(x => x.Email, "emailexemplo@gmail.com")
+                .Build();
+
+            _userRepository.SelectOne(Arg.Any<Expression<Func<Domain.Entities.User, bool>>>()).ReturnsForAnyArgs(user);
+
+            var act = async () => await _handler.Handle(request, new CancellationToken());
+
+            await act.Should().ThrowAsync<DuplicateValueException>().WithMessage("User already exists with the same email informed.");
+            await _userRepository.Received(1).SelectOne(Arg.Any<Expression<Func<Domain.Entities.User, bool>>>());
+            _mapper.DidNotReceive().Map<Address>(Arg.Any<CreateUserRequest>());
+            await _addressRepository.DidNotReceive().Create(Arg.Any<Address>());
+            _mapper.DidNotReceive().Map<Domain.Entities.User>(Arg.Any<CreateUserRequest>());
+            await _userRepository.DidNotReceive().Create(Arg.Any<Domain.Entities.User>());
+            _jwtHandler.DidNotReceive().CreateToken(Arg.Any<Domain.Entities.User>());
+            _hashHandler.DidNotReceive().Hash(Arg.Any<string>());
         }
     }
 }
