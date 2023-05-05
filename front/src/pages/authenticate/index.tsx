7377b360871cf76d510ae8fd0e18dac2a8a131a3
Luiz-Kohler <luizfelipekohler03@gmail.com>
2023-05-04 21:44:03 -0300

    creating auth page structure

---
+++ front/src/pages/authenticate/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css'
+
+const Authenticate: React.FC = () => {
+    return (
+        <div className='container'>
+
+        </div>
+    )
+};
+
+export default Authenticate;
--- front/src/routes.tsx
+++ front/src/routes.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom'
 
 import Home from './pages/home'
+import Authenticate from './pages/authenticate'
 
 const PageRoutes = () => {
     return (
         <Routes>
             <Route path="/home" element={<Home />}/>
+            <Route path="/authenticate" element={<Authenticate />}/>
             <Route path="/pets" element={<h1>pets</h1>}/>
-            <Route path="/authenticate" element={<h1>authenticate</h1>}/>
             <Route path='*' element={<Home />}/>
         </Routes>
     )
